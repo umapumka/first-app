0c2a6c900860f85ef69787f47d5ee9ae42bef628
Umutay <taytayka15@gmail.com>
2024-06-19 23:15:37 -0500

    everything updated ,but i don't know how to make it work

---
+++ books.js
@@ -0,0 +1,142 @@
+export const books = [
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        year: 1925,
+        cover: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/The_Great_Gatsby_cover_1925_wikisource.jpg/640px-The_Great_Gatsby_cover_1925_wikisource.jpg",
+        genre: "Classic"
+    },
+    {
+        title: "Brave New World",
+        author: "Aldous Huxley",
+        year: 1932,
+        cover: "https://i.ebayimg.com/images/g/MJQAAOSwgYpdUI1f/s-l1600.jpg",
+        genre: "Fiction"
+    },
+    {
+        title: "Catch-22",
+        author: "Joseph Heller",
+        year: 1961,
+        cover: "https://upload.wikimedia.org/wikipedia/en/9/99/Catch22.jpg",
+        genre: "Satire"
+    },
+    {
+        title: "Slaughterhouse-Five",
+        author: "Kurt Vonnegut",
+        year: 1969,
+        cover: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781684156252/slaughterhouse-five-9781684156252_hr.jpg",
+        genre: "Fiction"
+    },
+    {
+        title: "The Road",
+        author: "Cormac McCarthy",
+        year: 2006,
+        cover: "https://i.pinimg.com/originals/00/19/44/001944b53f07203d7b5fe89377de593a.jpg",
+        genre: "Dystopian"
+    },
+    {
+        title: "The Bell Jar",
+        author: "Sylvia Plath",
+        year: 1963,
+        cover: "https://www.thenovelhermit.com/wp-content/uploads/2013/02/sp-bj.jpg",
+        genre: "Fiction"
+    },
+    {
+        title: "The Picture of Dorian Gray",
+        author: "Oscar Wilde",
+        year: 1890,
+        cover: "https://images.booksense.com/images/272/716/9798736716272.jpg",
+        genre: "Gothic"
+    },
+    {
+        title: "One Hundred Years of Solitude",
+        author: "Gabriel García Márquez",
+        year: 1967,
+        cover: "https://s26162.pcdn.co/wp-content/uploads/2018/02/12084166.jpg",
+        genre: "Magical Realism"
+    },
+    {
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        year: 1813,
+        cover: "https://shop.livyluxe.com/wp-content/uploads/2024/03/pride-and-prejudice.jpg",
+        genre: "Romance"
+    },
+    {
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        year: 1951,
+        cover: "https://i0.wp.com/www.printmag.com/wp-content/uploads/2010/02/2326a7_e6d8ea079e4344e791725f50fbb89a94mv2.jpg?resize=263%2C388&quality=89&ssl=1",
+        genre: "Coming-of-age"
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960,
+        cover: "https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UF1000,1000_QL80_.jpg",
+        genre: "Fiction"
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        year: 1949,
+        cover: "https://www.theoriginalunderground.com/cdn/shop/products/1984-george-orwell-cover-print-179850_1024x.jpg?v=1656593374",
+        genre: "Dystopian"
+    },
+    {
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        year: 1954,
+        cover: "https://i.redd.it/pu1i0ekgyhu81.jpg",
+        genre: "Fantasy"
+    },
+    {
+        title: "Harry Potter and the Philosopher's Stone",
+        author: "J.K. Rowling",
+        year: 1997,
+        cover: "https://m.media-amazon.com/images/I/81q77Q39nEL._AC_UF1000,1000_QL80_.jpg",
+        genre: "Fantasy"
+    },
+    {
+        title: "Moby-Dick",
+        author: "Herman Melville",
+        year: 1851,
+        cover: "https://m.media-amazon.com/images/I/616R20nvohL._AC_UF1000,1000_QL80_.jpg",
+        genre: "Adventure"
+    },
+    {
+        title: "War and Peace",
+        author: "Leo Tolstoy",
+        year: 1869,
+        cover: "https://images.booksense.com/images/934/447/9780140447934.jpg",
+        genre: "Fiction"
+    },
+    {
+        title: "The Hitchhiker's Guide to the Galaxy",
+        author: "Douglas Adams",
+        year: 1979,
+        cover: "https://ih1.redbubble.net/image.287357871.9979/fposter,small,wall_texture,square_product,600x600.u7.jpg",
+        genre: "Fiction"
+    },
+    {
+        title: "The Brothers Karamazov",
+        author: "Fyodor Dostoevsky",
+        year: 1880,
+        cover: "https://m.media-amazon.com/images/I/81IE8AwMvqL._AC_UF1000,1000_QL80_.jpg",
+        genre: "Philosophical Fiction"
+    },
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        year: 1937,
+        cover: "https://m.media-amazon.com/images/I/71V2v2GtAtL._AC_UF1000,1000_QL80_.jpg",
+        genre: "Fantasy"
+    },
+    {
+        title: "The Little Prince",
+        author: "Antoine de Saint-Exupéry",
+        year: 1943,
+        cover: "https://cdn.kobo.com/book-images/6a33c46b-8bca-4b8e-9bd5-de603a032b49/1200/1200/False/the-little-prince-43.jpg",
+        genre: "Children's Literature"
+    }
+];
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*
+js of "convert"
+
+
 function convertTemperature() {
 
 
@@ -5,11 +9,12 @@ function convertTemperature() {
     const ConvertForm = document.getElementById('ConvertForm').value;
     const convertTo = document.getElementById('convertTo').value;
 
-    
+
     let result;
 
+
     if (ConvertForm === convertTo) {
-        result = celsiusInput;
+        result = parseInt(celsiusInput);
     } else if (ConvertForm === 'C' && convertTo === 'F') {
         result = (celsiusInput * 9/5) + 32;
     } else if (ConvertForm === 'F' && convertTo === 'C') {
@@ -18,3 +23,94 @@ function convertTemperature() {
 
     document.getElementById('showResult').innerText = `${parseFloat(result).toFixed(2)}°${convertTo}`;
 }
+
+document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    convertTemperature()
+}*/
+
+/*const users = [
+    {
+        id: 1,
+        fullname: "John Doe"
+    },
+    {
+        id: 2,
+        fullname: "George Smith"
+    },
+    {
+        id: 3,
+        fullname: "Angelina Jolie"
+    },
+    {
+        id: 4,
+        fullname: "Sponge Bob"
+    },
+];
+function getUser(fullname) {
+    const getUserPromise = new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            const foundUser = users.find(user => user.fullname === fullname);
+            
+            if (foundUser !== undefined) {
+                resolve(foundUser);
+            }else{
+                reject(`User with id ${fullname} doesn't exist.`);
+            }
+        }, 2000);
+    });
+
+    return getUserPromise;
+}
+
+getUser("Sponge Bab").then((result) =>{
+    console.log(result);
+
+})
+
+.catch((error) => {
+    console.error(error);
+})*/
+
+//books js
+
+import { books } from "./books.js";
+
+const booksContainer = document.getElementById("allBooks")
+
+books.forEach(book => {
+    const bookDiv = document.createElement("div")
+    bookDiv.classList.add("bookCard");
+
+    const img = document.createElement("img")
+    img.src = book.cover;
+    img.alt = book.title;
+
+    bookDiv.appendChild(img);
+
+    const detailsDiv =document.createElement("div");
+    detailsDiv.classList.add("detailsDiv");
+
+    const title = document.createElement("h4");
+    title.classList.add("book-title");
+    title.textContent = book.title;
+    detailsDiv.appendChild(title);
+
+    const authorName = document.createElement("p");
+    authorName.classList.add("author");
+    authorName.textContent = book.author;
+    detailsDiv.appendChild(authorName)
+
+    bookDiv.append(detailsDiv)
+    booksContainer.append(bookDiv)
+
+   
+});
+
+
+
+
+
+
